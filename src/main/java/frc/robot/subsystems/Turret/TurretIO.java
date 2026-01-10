c49d25995cae35e4ee0625633a1baa4e2dbd1b7b
Warblingpants67 <78549046+Warblingpants67@users.noreply.github.com>
2026-01-10 16:39:11 -0600

    Finish TurretIO Interface

--- src/main/java/frc/robot/subsystems/Turret/TurretIO.java
+++ src/main/java/frc/robot/subsystems/Turret/TurretIO.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.Turret;
 
 public interface TurretIO {
+    class TurretIOInputs {
+        public double turretRadians;
+        public double hoodRadians;
+        public double leftFlywheelRPM;
+        public double rightFlywheelRPM;
+    }
 
+    default void updateInputs(TurretIOInputs inputs) {}
+    default void setTurretRadians(double radians) {}
+    default void setHoodRadians(double radians) {}
+    default void setLeftFlywheelRPM(double rpm) {}
+    default void setRightFlywheelRPM(double rpm) {}
 }
