0583ac98de2b50aaaa220a1e63aac939bf654320
AllenRoboMasters <voss0266@umn.edu>
2026-01-11 15:38:29 -0600

    Variable name change

--- src/main/java/frc/robot/subsystems/Turret/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret/Turret.java
@@ -43,12 +43,12 @@ public class Turret extends SubsystemBase {
         turntable.setTargetRadians(angleToFaceGoalLocal);
 
         double velocity_initial_y = Math.sqrt(-2 * TurretConstants.GRAVITY_CONSTANT * (TurretConstants.TURRET_VERTICAL_DISTANCE_TO_GOAL + TurretConstants.TURRET_VERTICAL_DISTANCE_APEX_OFFSET));
-        double time_at_apex = -velocity_initial_y / TurretConstants.GRAVITY_CONSTANT;
+        double time_until_apex = -velocity_initial_y / TurretConstants.GRAVITY_CONSTANT;
         double delta_x = Math.sqrt(dx * dx + dy * dy);
         
-        delta_x -= (TurretConstants.TURRET_TIME_INTO_GOAL_AFTER_APEX / (time_at_apex + TurretConstants.TURRET_TIME_INTO_GOAL_AFTER_APEX)) * delta_x;
+        delta_x -= (TurretConstants.TURRET_TIME_INTO_GOAL_AFTER_APEX / (time_until_apex + TurretConstants.TURRET_TIME_INTO_GOAL_AFTER_APEX)) * delta_x;
 
-        double velocity_inital_x = delta_x / time_at_apex;
+        double velocity_inital_x = delta_x / time_until_apex;
 
         double launch_angle = Math.atan2(velocity_initial_y, velocity_inital_x);
         double launch_velocity = Math.sqrt(velocity_inital_x * velocity_inital_x + velocity_initial_y * velocity_initial_y);
